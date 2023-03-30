4cc9e05099af3a6383e900b0f11820fe3153c145
BernardOnuh <onuhbernard4@gmail.com>
2023-03-30 10:58:56 -0700

    dex

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import Link from 'next/link';
+import { ConnectWallet } from '@thirdweb-dev/react'
 
 const Header = () => {
         return(
             <header>
                 <div className='leftH'>
-                    
+                    <img src='/Logo' className='logo'/>
+                    <Link className='link' href='/'>
+                        <div className='headerItem'>Swap</div>
+                    </Link>
+                    <Link href='/tokens'>
+                    <div className='headerItem'></div>
+                    </Link>
+                </div>
+                <div className='rightH'>
+                    <div className='headerItem'>
+                        <img src='/Eth' alt='eth' className='eth'/>
+                        Ethereum
+                    </div>
+                    <div>
+                        <ConnectWallet/>
+                    </div>
                 </div>
             </header>
         );
 };
+
+export default Header
