0e776959f5c2c7d02559b1bfed6c6fa5479ec960
BernardOnuh <onuhbernard4@gmail.com>
2023-04-05 13:32:32 -0700

    api

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -3,10 +3,15 @@ import *as dotenv from 'dotenv';
 import { EvmChain } from '@moralisweb3/common-evm-utils';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+dotenv.config()
 export default async function runApp(
   req: NextApiRequest,
   res: NextApiResponse
 ){
+  await Moralis.start({
+    apiKey: process.env.MORALIS_KEY,
+  });
+  
   const address = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599";
 
   const chain = EvmChain.ETHEREUM;
@@ -16,9 +21,8 @@ export default async function runApp(
     chain,
   });
 
-  console.log(response.toJSON());
+  return res.status(200).json(response);
 
-  Moralis.start({
-    apiKey: process.env.MORALIS_KEY,
-  });
+
+  console.log(response.toJSON());
 };
